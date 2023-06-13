97ae5d06a7eee69f394e99dd7c70a276017577e4
ywrth <zuatworknow@gmail.com>
2023-06-13 13:01:40 +0200

    New code / Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-alert('Hello world');
-let favoriteFood = 'Pasta';
-document.write (favoriteFood);
+let pokemonList = [ //POKEMONLIST//
+    {
+        name: 'Bulbasaur',
+        id: 1,
+        type: ['grass', 'poison'],
+        height: 7
+    },
+    {
+        name: 'Machoke',
+        id: 2,
+        type: ['fighting'],
+        height: 1.5
+    },
+    {
+        name: 'Crobat',
+        id: 3,
+        type: ['poison', 'flying'],
+        height: 1.8
+    },
+    {
+        name: 'Lillipup>',
+        id: 4,
+        type: ['normal'],
+        height: 0.4
+    },
+]
