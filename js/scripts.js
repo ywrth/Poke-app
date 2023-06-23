f88637e21ac7d405d31179e96e12f8534d5b8f48
ywrth <zuatworknow@gmail.com>
2023-06-23 10:25:15 +0200

    forEach / get.All / function

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,54 @@
-
-newFunction();  
-
-function newFunction() {
+let pokemonRepository = (function () {
     let pokemonList = [
-        {
-            name: 'Bulbasaur',
-            id: 1,
-            type: ['grass', 'poison'],
-            height: 7
-        },
-        {
-            name: 'Machoke',
-            id: 2,
-            type: ['fighting'],
-            height: 1.5
-        },
-        {
-            name: 'Crobat',
-            id: 3,
-            type: ['poison', 'flying'],
-            height: 1.8
-        },
-        {
-            name: 'Lillipup',
-            id: 4,
-            type: ['normal'],
-            height: 0.4
-        }
+      {
+        name: 'Bulbasaur',
+        id: 1,
+        type: ['grass', 'poison'],
+        height: 7
+      },
+      {
+        name: 'Machoke',
+        id: 2,
+        type: ['fighting'],
+        height: 1.5
+      },
+      {
+        name: 'Crobat',
+        id: 3,
+        type: ['poison', 'flying'],
+        height: 1.8
+      },
+      {
+        name: 'Lillipup',
+        id: 4,
+        type: ['normal'],
+        height: 0.4
+      }
     ];
-    
-    for (let i = 0; i < pokemonList.length; i++) {
-        if (pokemonList[i].height < 6 && pokemonList[i].height > 0.5) {
-            document.write(pokemonList[i].name + " is super tall (height: " + pokemonList[i].height + ")<br>");
-        } else if (pokemonList[i].height < 0.5) {
-            document.write(pokemonList[i].name + " is tiny (height: " + pokemonList[i].height + ")<br>");
-        } else {
-            document.write(pokemonList[i].name + " is huge (height: " + pokemonList[i].height + ")");
-            if (pokemonList[i].height > 5) {
-                document.write(" - WOW THAT'S BIG!<br> ");
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    pokemonList.forEach(function (pokemon) {
+      if (pokemon.height < 6 && pokemon.height > 0.5) {
+        document.write(pokemon.name + " is super tall (height: " + pokemon.height + ")<br>");
+      } else if (pokemon.height < 0.5) {
+        document.write(pokemon.name + " is tiny (height: " + pokemon.height + ")<br>");
+      } else {
+        document.write(pokemon.name + " is huge (height: " + pokemon.height + ")");
+        if (pokemon.height > 5) {
+          document.write(" - WOW THAT'S BIG!<br> ");
         }
-    }
-    }
-}
+      }
+    });
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
